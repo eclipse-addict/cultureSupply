4008c266ccbeaa542022eb43d1c720e288176f61
isaac <isaacshin92@gmail.com>
2022-09-13 00:48:12 +0900

    커뮤니티 게시판 구성 중, 프로필 사진 추가예정.nav 수정

--- articles/views.py
+++ articles/views.py
@@ -4,3 +4,17 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'articles/index.html')
+
+def detail(request, pk):
+    pass
+
+def create(request):
+    pass
+
+def update(request, pk):
+    pass
+
+def delete(request, pk):
+    pass
+
+
