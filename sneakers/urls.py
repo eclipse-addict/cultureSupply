f8de091ce5368c84a439d2e9118aa1d0178ef42a
isaac <isaacshin92@gmail.com>
2022-10-22 00:49:40 +0900

    Main index API- recent posts + like_user Serialize

--- sneakers/urls.py
+++ sneakers/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 app_name = 'sneakers'
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.index),
     path('create/', views.create, name='create'),
     path('<int:pk>/update/', views.update, name='update'),
     path('<int:pk>/', views.detail, name='detail'),
