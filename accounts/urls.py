3ec58e572b9b6b6366fca13ad2b28ce09bcbb1fb
신기욱 전임교수 <isaacshin92@gmail.com>
2023-04-13 09:39:55 +0900

    SMTP 서버 구성 및 설정 변경

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,18 +4,22 @@ from rest_framework_simplejwt.views import TokenRefreshView
 from dj_rest_auth.registration.views import (
     VerifyEmailView, RegisterView
 )
-from .views import ConfirmEmailView
+from .views import ConfirmEmailView, ResendConfirmationView 
 
 
 urlpatterns = [
     # re_path(r'^dj-rest-auth/registration/account-confirm-email/(?P<key>.+)/$', views.confirm_email),
     path('dj-rest-auth/', include('dj_rest_auth.urls')),
     # path('dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')),
-        # 이메일 관련 필요
+    
+    # 이메일 관련 필요
     path('accounts/allauth/', include('allauth.urls')),
+    
     # 유효한 이메일이 유저에게 전달
     re_path(r'^account-confirm-email/$', VerifyEmailView.as_view(), name='account_email_verification_sent'),
-    path('rest-auth/registration', RegisterView.as_view(), name='rest_register'),
+    path('resend-confirmation/', ResendConfirmationView.as_view(), name='resend_confirmation'),
+
+    path('rest-auth/registration/', RegisterView.as_view(), name='rest_register'),
     re_path(r'^dj-rest-auth/registration/account-confirm-email/(?P<key>[-:\w]+)/$', ConfirmEmailView.as_view(), name='account_confirm_email'),
     path('userinfo/get/<int:user_pk>/', views.get_update_userInfo),
     path('nickcheck/', views.nick_name_check),
--- accounts/views.py
+++ accounts/views.py
@@ -18,20 +18,31 @@ from allauth.account.models import EmailConfirmation, EmailAddress
 from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_exempt
 from dj_rest_auth.registration.views import ConfirmEmailView
+from django.urls import reverse_lazy
+from django.views.generic import View
+from django.shortcuts import redirect
+from django.template.loader import render_to_string
+from django.core.mail import send_mail
+from django.conf import settings
+from django.contrib import messages
 
-@api_view(['POST',])
-@permission_classes([AllowAny])
-def create_userInfo(request, user_pk):
-    user = get_object_or_404(User, pk=user_pk)
+User = get_user_model()
+
+
+
+# @api_view(['POST',])
+# @permission_classes([AllowAny])
+# def create_userInfo(request, user_pk):
+#     user = get_object_or_404(User, pk=user_pk)
     
-    if request.method == 'POST':
-        serializer = UserInfoSerializer(data=request.data)
+#     if request.method == 'POST':
+#         serializer = UserInfoSerializer(data=request.data)
         
-        if serializer.is_valid(raise_exception=True):
-            serializer.save(user=user)
-            new_user_point(user_pk)
+#         if serializer.is_valid(raise_exception=True):
+#             serializer.save(user=user)
+#             new_user_point(user_pk)
             
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
+#             return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 
 
@@ -70,7 +81,7 @@ def email_check(request):
         return HttpResponse(status=status.HTTP_226_IM_USED)
     return HttpResponse(status=status.HTTP_200_OK)
 
-    
+# 이메일 인증 관련    
 class ConfirmEmailView(APIView):
     permission_classes = [AllowAny]
 
@@ -78,7 +89,24 @@ class ConfirmEmailView(APIView):
         self.object = confirmation = self.get_object()
         print(confirmation)
         confirmation.confirm(self.request)
-        return HttpResponseRedirect('/login/success/')
+        
+        # 이메일 인증이 성공한 시점에, 유저정보 테이블을 생성한다. 
+        info_table_created = self.create_userInfo()
+        if info_table_created:
+            return HttpResponseRedirect('https://www.kickin.kr/')
+        # 이메일 인증이 성공한 뒤, 이메일 인증이 성공했다는 화면으로 이동. 
+        return HttpResponseRedirect('https://www.kickin.kr/404/')
+    def create_userInfo(self):
+        key = self.kwargs['key']
+        user = EmailConfirmationHMAC.from_key(key).email_address.user
+        userInfo = UserInfo.save(user=user, first_name=user.first_name, last_name=user.last_name, nick_name=user.nick_name)
+        new_user_point(user.pk)
+        
+        if userInfo: 
+            return True
+        
+        return False
+
 
     def get_object(self, queryset=None):
         key = self.kwargs['key']
@@ -98,3 +126,55 @@ class ConfirmEmailView(APIView):
         qs = EmailConfirmation.objects.all_valid()
         qs = qs.select_related("email_address__user")
         return qs
+    
+    
+
+@method_decorator(csrf_exempt, name='dispatch')
+class ResendConfirmationView(View):
+    # success_url = reverse_lazy('account_email_verification_sent')
+    email_template_name = 'registration/verification_email.html'
+    # subject_template_name = 'registration/verification_email_subject.txt'
+
+    def post(self, request, *args, **kwargs):
+        email = request.POST.get('email')
+        user = User.objects.get(email=email)
+        user_email = EmailAddress.objects.get(email=email)
+        verification_key = EmailAddress.objects.get(email=email)
+        
+        print('email : ',request.POST.get('email'))
+        print('user : ', user)
+        print('verification_key : ', verification_key)
+        if user_email.verified:
+            print('user.is_active : ', user.is_active)
+            data = {
+                'message': '이미 이메일 주소 인증이 확인되었습니다.'
+            }
+            return HttpResponse(content=data, status=status.HTTP_200_OK)
+
+        key = EmailConfirmationHMAC(user.emailaddress_set.get(email=user.email)).key
+        print()
+        context = {
+            'user': user,
+            # /user/dj-rest-auth/registration/account-confirm-email/
+            'activate_url': request.build_absolute_uri(reverse_lazy('account_confirm_email', kwargs={'key': key})),
+        }
+        message = render_to_string(self.email_template_name, context)
+        send_mail(
+            subject='[Kickin] 이메일 주소 확인',
+            message='',
+            html_message=message,
+            from_email=settings.DEFAULT_FROM_EMAIL,
+            recipient_list=[email],
+            fail_silently=False,
+        )
+        data = {
+            'msg': '확인 이메일이 이메일 주소로 전송되었습니다.',
+            'status_code': '200'
+            }
+        return JsonResponse(data=data, status=status.HTTP_200_OK)
+
+
+
+
+
+
